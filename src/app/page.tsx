a3a2a334d73d5556531e281bf88add664b06b40e
Denys1994q <drybachok@gmail.com>
2023-08-21 16:38:22 +0300

    feat: add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import './page.module.sass'
+import styles from './page.module.sass'; 
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.k}>
     Home Page 1
-    </>
+    </div>
   )
 }
